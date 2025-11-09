fd03448c39e19a845db1b9ff67956369d7621897
Nate Rohweder <rohwedernt@gmail.com>
2025-11-08 19:46:24 -0700

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,7 +20,7 @@ export const Navbar = () => {
         justify="start"
       >
         <NavbarItem className="hidden sm:flex">
-          <Button onPress={() => window.history.back()} variant="light" size="lg" startContent={<FaArrowLeft />}>Back</Button>
+          <Button onClick={() => window.history.back()} variant="light" size="lg" startContent={<FaArrowLeft />}>Back</Button>
         </NavbarItem>
       </NavbarContent>
 
